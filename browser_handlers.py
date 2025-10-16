9796a9f3a4e22b2557f23d61a99bafaee87758b7
Alex45452 <youtube.no@mail.ru>
2025-10-16 16:01:33 +0300

    wb orders v0.0.1

--- browser_handlers.py
+++ browser_handlers.py
@@ -11,6 +11,10 @@ def create_order(page):
     page.wait_for_selector("label:has-text('Выбрать все')")
     page.locator("label:has-text('Выбрать все')").click()
     page.locator("label.list-item__checkbox").first.click()
+    if page.locator("span.basket-order__link").count() == 1:
+        page.locator("span.basket-order__link").click()
+        page.locator("span.address-item__name-text:has-text('поселение Воскресенское,   40к1')").click()
+        page.get_by_role("button", name="Заберу отсюда").click()
     page.get_by_role("button", name="Заказать").click()
 
 def check_order(page):
@@ -25,24 +29,25 @@ def browser_close(context,browser):
     browser.close()
 
 def order_handler():
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=False)
+        context = browser.new_context()
 
-    browser = sync_playwright().chromium.launch(headless=False)
-    context = browser.new_context()
+        context.add_cookies(settings.cookies)
+        
+        page = context.new_page()
+        page.goto(MAIN_PAGE_URL)
 
-    context.add_cookies(settings.cookies)
-    
-    page = context.new_page()
-    page.goto(MAIN_PAGE_URL)
+        page.evaluate(f"""
+                localStorage.setItem('wbx__tokenData', '{json.dumps(settings.wbx_token)}');
+                """)
+        page.reload()
+        print(page)
 
-    page.evaluate(f"""
-            localStorage.setItem('wbx__tokenData', '{json.dumps(settings.wbx_token)}');
-            """)
-    page.reload()
-    print(page)
-
-    create_order(page)
-    check_order(page)
-    browser_close(context,browser)
+        create_order(page)
+        check_order(page)
+        browser_close(context,browser)
 
 if __name__ == "__main__":
     print("If you see this, u MUST be in a debug session.\nCheck what file you are running!")
+    order_handler()
