6d4bfc17ac95e167fdd3ecc184e0908bf69b19ab
Alex45452 <youtube.no@mail.ru>
2025-10-19 20:23:10 +0300

    v0.0.4 wb offer

--- browser_handlers.py
+++ browser_handlers.py
@@ -1,61 +1,68 @@
-from playwright.sync_api import sync_playwright
+from playwright.async_api import async_playwright
 from requests_handlers import add_to_cart_handler
 from settings import cookies, wbx_token, cur_address
 import json
 import time
+import asyncio
 
 MAIN_PAGE_URL = "https://wildberries.ru"
 BASKET_URL = "https://wildberries.ru/lk/basket"
 
 
-def create_order(page):
-    page.goto(BASKET_URL)
-    page.wait_for_selector("label:has-text('Выбрать все')")
-    page.locator("label:has-text('Выбрать все')").click()
-    page.locator("label.list-item__checkbox").first.click()
-    if page.locator("span.basket-order__link").count() == 1:
-        page.locator("span.basket-order__link").click()
-        page.locator(f"span.address-item__name-text:has-text('{cur_address}')").click()
-        page.get_by_role("button", name="Заберу отсюда").click()
-    page.get_by_role("button", name="Заказать").click()
-    page.wait_for_selector("button.popup__btn-main", timeout=1000)
-    if page.locator("button.popup__btn-main", has_text="Да, заказать").count() == 1:
-        page.click("button.popup__btn-main")
+async def create_order(page):
+    await page.goto(BASKET_URL)
+    await page.wait_for_selector("div.basket-form__basket-section.basket-section", timeout=20000)
+    await asyncio.sleep(1)
+    if await page.locator("label:has-text('Выбрать все')").count() == 1:
+        await page.locator("label:has-text('Выбрать все')").click()
+        await page.locator("label.list-item__checkbox").first.click()
+    if await page.locator("span.basket-order__link").count() == 1:
+        await page.locator("span.basket-order__link").click()
+        await page.locator(f"span.address-item__name-text:has-text('{cur_address}')").click()
+        await page.get_by_role("button", name="Заберу отсюда").click()
+    await page.get_by_role("button", name="Заказать").click()
+    bank =  page.locator("li.popup__banks-item:has-text('ПСБ')")
+    if await bank.count() == 1:
+        await bank.click()
+    if await page.locator("button.btn-main", has_text="Да, заказать").count() == 1:
+        await page.click("button.btn-main")
+    if await page.locator("button.popup__btn-main", has_text="Да, заказать").count() == 1:
+        await page.click("button.popup__btn-main")
 
-def check_order(page):
+async def check_order(page):
     ...
     # with page.expect_request("**/data_v2**") as req_info:
     #     page.locator("button.j-btn-confirm-order").click()
     # request = req_info.value
     # print("Отправлен запрос:", request.url)
 
-def browser_close(context,browser):
-    context.close()
-    browser.close()
+async def browser_close(context,browser):
+    await context.close()
+    await browser.close()
 
-def order_handler():
-    with sync_playwright() as p:
-        browser = p.chromium.launch(headless=True)
-        context = browser.new_context()
+async def order_handler():
+    async with async_playwright() as p:
+        browser = await p.chromium.launch(headless=False)
+        context = await browser.new_context()
 
-        context.add_cookies(cookies)
+        await context.add_cookies(cookies)
         
-        page = context.new_page()
-        page.goto(MAIN_PAGE_URL)
+        page = await context.new_page()
+        await page.goto(MAIN_PAGE_URL)
 
-        page.evaluate(f"""
+        await page.evaluate(f"""
                 localStorage.setItem('wbx__tokenData', '{json.dumps(wbx_token)}');
                 """)
-        page.reload()
+        await page.reload()
         print(page)
+        await create_order(page)
+        await check_order(page) # todo
+        await browser_close(context,browser)
 
-        create_order(page)
-        check_order(page) # todo
-        browser_close(context,browser)
 
 if __name__ == "__main__":
     print("If you see this, u MUST be in a debug session.\nCheck what file you are running!")
     start = time.time()
-    if add_to_cart_handler(516341146):
-        order_handler()
+    if add_to_cart_handler(283031138):
+        asyncio.run(order_handler())
     print(time.time()-start)
