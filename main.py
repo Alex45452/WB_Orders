87c9c6f305d9c48a1628f1ba499006414a890dca
Alex45452 <youtube.no@mail.ru>
2026-01-28 04:43:39 +0300

    orders notification fixed

--- main.py
+++ main.py
@@ -66,7 +66,7 @@ def check_created_order(event):
 
 async def msg_processing(event):
     if check_created_order(event):
-        client.send_message(cur_recipient,"Заказ создан в боте, проверьте свой аккаунт")
+        client.send_message(CHANNEL_ID,"Заказ создан в боте, проверьте свой аккаунт")
         return
     elif check_call_conditions(event):
         return 
